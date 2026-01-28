9200ca2f217dcff3edb870fd79e73bbdbe3c50cb
Vitaliy Kozhemyak <vitaliy.kozhemyak@gmail.com>
2026-01-28 19:51:11 +0300

    mvp

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,3 @@
-import postgres from 'postgres';
 import {
   CustomerField,
   CustomersTableType,
@@ -7,21 +6,20 @@ import {
   LatestInvoiceRaw,
   Revenue,
 } from './definitions';
+import sql from './postgres-client';
 import { formatCurrency } from './utils';
-
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
